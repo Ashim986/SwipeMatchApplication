806b85a5fea924b10126fe820c734d170adc1d10
Ashim Dahal <ashim.dahal@united.com>
2018-12-18 00:37:31 -0600

    Initial Commit

---
+++ SwipeMatchApplication/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwipeMatchApplication
+//
+//  Created by ashim dahal on 12/18/18.
+//  Copyright © 2018 ashim dahal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       view.backgroundColor = .red
+        
+    }
+
+
+}
+
