a9df4432cfe3eea3b234a9243f6433dcd9a6aad5
Ashim Dahal <ashim.dahal@united.com>
2018-12-30 16:52:35 -0600

    hoem controller arrange view in stack view

---
+++ SwipeMatchApplication/HomeControllerViews/HomeNavigationStackView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeNavigationStackView.swift
+//  SwipeMatchApplication
+//
+//  Created by ashim dahal on 12/30/18.
+//  Copyright © 2018 ashim dahal. All rights reserved.
+//
+
+import UIKit
+
+class HomeNavigationStackView: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
