f38e5b0a283ad8495352770858609b33b56bc67d
Ashim Dahal <ashim.dahal@united.com>
2018-12-30 16:13:09 -0600

    Image Asset stackViewController arrangement

--- SwipeMatchApplication/ViewController.swift
+++ SwipeMatchApplication/ViewController.swift
@@ -10,40 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let footerStackView = HomeBottomControlStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         let headerViews = [UIColor.gray, UIColor.lightGray, UIColor.darkGray].map { (color) -> UIView in
             let view = UIView()
-            view.backgroundColor = .clear
+            view.backgroundColor = color
             return view
         }
-     
+        
+        
         let headerStackView = UIStackView(arrangedSubviews: headerViews)
         headerStackView.distribution = .fillEqually
         headerStackView.backgroundColor = .red
         headerStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
         
         
-        let footerViews = [UIColor.red, UIColor.black, UIColor.green, UIColor.yellow, UIColor.lightGray].map { (bgcolor) -> UIView in
-            let view = UIView()
-            view.backgroundColor = bgcolor
-            return view
-        }
-        let footerStackView = UIStackView(arrangedSubviews: footerViews)
-        footerStackView.backgroundColor = .yellow
-        footerStackView.distribution = .fillEqually
-        footerStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
         
         let blueView = UIView()
         blueView.backgroundColor = .blue
         
         let stackView = UIStackView(arrangedSubviews: [headerStackView, blueView, footerStackView])
         stackView.axis = .vertical
-        
         view.addSubview(stackView)
         
-       stackView.fillSuperView()
+        stackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
 
     }
     
